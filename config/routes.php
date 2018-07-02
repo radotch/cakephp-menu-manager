40c99cb02c3b826211cc312e805567766c327fcd
Radoslav Cholakov <tcholakov_radoslav@yahoo.com>
2018-07-02 19:58:59 +0300

    Update routes definition.

--- config/routes.php
+++ config/routes.php
@@ -10,3 +10,15 @@ Router::plugin(
         $routes->fallbacks(DashedRoute::class);
     }
 );
+
+Router::prefix('admin', [], function (RouteBuilder $routes) {
+    $routes->plugin(
+        'MenuManager',
+        ['path' => '/menu-manager'],
+        function (RouteBuilder $routes) {
+            $routes->connect('/', ['controller' => 'Menus', 'action' => 'index']);
+
+            $routes->fallbacks(DashedRoute::class);
+        });
+    $routes->fallbacks(DashedRoute::class);
+});
