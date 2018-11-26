b5cc801c5b11f17d0ed9538b303fc922f1cc9e70
Radoslav Cholakov <tcholakov_radoslav@yahoo.com>
2018-11-26 21:20:45 +0200

    Removed columns from I18n Migration.

--- config/Migrations/20181123130237_CreateI18n.php
+++ config/Migrations/20181123130237_CreateI18n.php
@@ -56,14 +56,6 @@ class CreateI18n extends AbstractMigration
             'limit' => 255,
             'null' => false,
         ]);
-        $table->addColumn('created', 'datetime', [
-            'default' => null,
-            'null' => false,
-        ]);
-        $table->addColumn('modified', 'datetime', [
-            'default' => null,
-            'null' => false,
-        ]);
         $table->create();
     }
 }
