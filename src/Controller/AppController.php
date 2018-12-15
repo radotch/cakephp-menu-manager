6598f2e0267cefa6d3690b0065a551d84a5b5681
Radoslav Cholakov <tcholakov_radoslav@yahoo.com>
2018-12-16 00:48:04 +0200

    Update method to remove default locale when present as key or value.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -9,6 +9,8 @@ class AppController extends BaseController
 {
     /**
      * Read translation Languages from Configuration.
+     * When default locale presents in languages as key or value will be removed
+     * from list
      * 
      * $param none
      * @return array Array with languages or empty array. 
@@ -16,6 +18,17 @@ class AppController extends BaseController
     protected function _getTranslationLanguages()
     {
         $languages = Configure::read('MenuManager.Translation.Languages', []);
+        $default = \Cake\I18n\I18n::getDefaultLocale();
+        
+        if (array_key_exists($default, $languages)) {
+            unset($languages[$default]);
+        }
+        
+        if (in_array($default, $languages)) {
+            $key = array_search($default, $languages);
+            
+            unset($languages[$key]);
+        }
         
         return $languages;
     }
