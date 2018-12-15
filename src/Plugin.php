8905075338e08f99e1a744f9c30a0070a14ece39
Radoslav Cholakov <tcholakov_radoslav@yahoo.com>
2018-12-15 22:25:30 +0200

    Update method description.

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,10 +3,31 @@
 namespace MenuManager;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\Configure;
 
 /**
  * Plugin for MenuManager
  */
 class Plugin extends BasePlugin
 {
+    /**
+     * Set Translation Languages. 
+     * For better results it's have to be associative array / list as next:
+     * ```
+     * [
+     *      'bg_BG' => 'Bulgarian',
+     *      'it_IT' => 'Italiano',
+     *      'fr_FR' => 'Francias',
+     * ]
+     * ```
+     * 
+     * @param array $languages Language list
+     * @return $this 
+     */
+    public function setTranslatons(array $languages = [])
+    {
+        Configure::write('MenuManager.Translation.Languages', $languages);
+        
+        return $this;
+    }
 }
